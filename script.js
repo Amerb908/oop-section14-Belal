fe421e75bcde566b939b1c76bbfb60c4812d2dc3
Amer Belal <ab1571514@gmail.com>
2024-08-09 18:21:10 -0700

    updated code

--- script.js
+++ script.js
@@ -55,6 +55,7 @@ console.log(rect2.area(), rect3.area());
 console.log(rect1.constructor);
 console.log(rect2 instanceof Rectangle);
 
+//----------------------------------------------------------------------------------------
 //Literals vs Built-in Constructors 03
 const strLit = 'Hello';
 const strObj = new String('Hello');
@@ -69,9 +70,95 @@ console.log(strLit[0]);
 //Unboxing
 console.log(strObj.valueOf(), typeof strObj.valueOf());
 
+console.log(strLit.constructor);
+console.log(strObj.constructor);
+
+console.log(strLit instanceof String);
+console.log(strObj instanceof String);
+
+//other types
+const numLit = 20;
+const numObj = new Number(20);
+
+consoile.log(numLit, typeof numLit);
+console.log(numObj, typeof numObj);
+
+const boolLit = true;
+const boolObj = new Boolean(true);
+
+console.log(boolLit, typeof boolLit);
+console.log(boolObj, typeof boolObj);
+
+const arrLit = [1, 2, 3];
+const arrObj = new Array(1, 2, 3);
+
+console.log(arrLit, typeof arrLit);
+console.log(arrObj, typeof arrObj);
+
+const funcLit = function(x) 
+{
+    return x * x;
+};
+
+console.log(funcLit, typeof funcLit);
+console.log(funcLit(10));
+
+const funcObj = new Function('x', 'return x * x');
+console.log(funcObj(3));
+
+const obj1 = {};
+const obj2 = new Object();
+
+console.log(obj1, typeof obj1);
+console.log(obj2, typeof obj2);
+
 //----------------------------------------------------------------------------------------
 //Working with Object Properties 04
+function Rectangle(name, width, height)
+{
+    this.name = name;
+    this.width = width;
+    this.height = height;
+    this.area = function()
+    {
+        return this.width * this.height
+    };
+}
 
+const rect1 = new Rectangle('Rectangle 1', 20, 10);
+const rect2 = new Rectangle('Rectangle 2', 30, 20);
+
+console.log(rect1.name, rect2.width);
+console.log(rect1.width);
+
+rect1.color = 'red';
+
+rect2.perimeter = () => 2 * (rect2.width + rect2.height);
+
+delete rect2.perimeter;
+
+//Check for property
+console.log(rect2.hasOwnProperty('color'));
+console.log(rect1.hasOwnProperty('color'));
+
+//check keys
+console.log(Object.keys(rect1));
+
+//check values
+console.log(Object.values(rect2));
+
+//check entries
+console.log(Object.entries(rect2));
+
+for (let [key, value] of Object.entries(rect1))
+{
+    if (typeof value !== 'function')
+    {
+        console.log(`${key} - ${value}`);
+    }
+}
+
+console.log(rect2);
 
 //----------------------------------------------------------------------------------------
 //Prototypes & The Prototype Chain 05
